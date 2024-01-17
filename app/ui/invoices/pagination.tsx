035139ba9044cd0a88477a5809515ef800a4da47
cristian gomez <1154@holbertonschool.com>
2024-01-17 15:13:05 -0500

    Add new files and fix formatting issues

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -18,7 +18,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     const params = new URLSearchParams(searchParams.toString());
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
-  }
+  };
 
   return (
     <>
